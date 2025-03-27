ba8b32d6be5c9f5b75c247ea724caa1519738dd4
Serge Kovbasiuk <isserge@protonmail.com>
2025-03-27 09:55:24 +0700

    Initialize main function with consensus and messaging modules, adding nodes to the network

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod consensus;
+mod messaging;
+mod state_machine;
+
+use std::sync::{Arc, Mutex};
+
+use consensus::Node;
+use messaging::Network;
+use state_machine::StateMachine;
+
 fn main() {
-    println!("Hello, world!");
+    let network = Arc::new(Mutex::new(Network::new()));
+
+    for id in 0..5 {
+        let node = Node::new(id, StateMachine::new());
+        network.lock().unwrap().add_node(node);
+    }
 }
