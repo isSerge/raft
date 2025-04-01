368c928265cb586d1f1bc0578bdb73bc720be944
Serge Kovbasiuk <isserge@protonmail.com>
2025-04-01 13:59:34 +0700

    NodeCore: add more unit tests

--- src/consensus/core.rs
+++ src/consensus/core.rs
@@ -374,11 +374,12 @@ impl NodeCore {
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn test_node_transition_to_candidate_and_vote_for_self() {
-        const TERM: u64 = 1;
-        const NODE_ID: u64 = 0;
 
+    const NODE_ID: u64 = 0;
+
+    #[test]
+    fn test_core_transition_to_candidate_and_vote_for_self() {
+        const TERM: u64 = 1;
         let mut core = NodeCore::new(NODE_ID);
 
         // check default values
@@ -395,8 +396,27 @@ mod tests {
     }
 
     #[test]
-    fn test_node_transition_to_follower_and_reset_voted_for() {
-        const NODE_ID: u64 = 0;
+    fn test_core_transition_to_candidate_does_not_transition_if_leader() {
+        const TERM: u64 = 1;
+
+        let mut core = NodeCore::new(NODE_ID);
+
+        core.transition_to_candidate();
+        core.transition_to_leader();
+
+        assert_eq!(core.state(), NodeState::Leader);
+        assert_eq!(core.current_term(), TERM);
+        assert_eq!(core.voted_for(), Some(NODE_ID));
+
+        core.transition_to_candidate();
+
+        assert_eq!(core.state(), NodeState::Leader);
+        assert_eq!(core.current_term(), TERM);
+        assert_eq!(core.voted_for(), Some(NODE_ID));
+    }
+
+    #[test]
+    fn test_core_transition_to_follower_and_reset_voted_for() {
         const TERM_0: u64 = 0;
         const TERM_1: u64 = 1;
         const TERM_2: u64 = 2;
@@ -424,8 +444,27 @@ mod tests {
     }
 
     #[test]
-    fn test_node_transition_to_leader() {
-        const NODE_ID: u64 = 0;
+    fn test_core_transition_to_follower_does_not_reset_for_same_term() {
+        const TERM_0: u64 = 0;
+
+        let mut core = NodeCore::new(NODE_ID);
+
+        // check default values
+        assert_eq!(core.state(), NodeState::Follower);
+        assert_eq!(core.current_term(), TERM_0);
+        assert_eq!(core.voted_for(), None);
+
+        core.transition_to_follower(TERM_0);
+
+        // check values after transition
+        assert_eq!(core.state(), NodeState::Follower);
+        assert_eq!(core.current_term(), TERM_0);
+        assert_eq!(core.voted_for(), None);
+        assert_eq!(core.votes_received(), 0);
+    }
+
+    #[test]
+    fn test_core_transition_to_leader() {
         const TERM_0: u64 = 0;
         const TERM_1: u64 = 1;
 
@@ -445,5 +484,88 @@ mod tests {
         assert_eq!(core.state(), NodeState::Leader);
         assert_eq!(core.current_term(), TERM_1); // term should increase
         assert_eq!(core.voted_for(), Some(NODE_ID));
+    }
+
+    #[test]
+    fn test_core_transition_to_leader_does_not_transition_if_not_candidate() {
+        const TERM: u64 = 0;
+
+        let mut core = NodeCore::new(NODE_ID);
+
+        // check default values
+        assert_eq!(core.state(), NodeState::Follower);
+        assert_eq!(core.current_term(), TERM);
+        assert_eq!(core.voted_for(), None);
+
+        core.transition_to_leader();
+
+        assert_eq!(core.state(), NodeState::Follower);
+        assert_eq!(core.current_term(), TERM);
+        assert_eq!(core.voted_for(), None);
+    }
+
+    #[test]
+    fn test_core_update_term_success() {
+        const TERM_0: u64 = 0;
+        const TERM_1: u64 = 1;
+
+        let mut core = NodeCore::new(NODE_ID);
+
+        // check default values
+        assert_eq!(core.current_term(), TERM_0);
+        assert_eq!(core.voted_for(), None);
+
+        // update term to 1
+        let term_updated = core.update_term(TERM_1);
+        assert!(term_updated);
+        assert_eq!(core.current_term(), TERM_1);
+        assert_eq!(core.voted_for(), None);
+    }
+
+    #[test]
+    fn test_core_update_term_failure() {
+        const TERM_0: u64 = 0;
+
+        let mut core = NodeCore::new(NODE_ID);
+
+        // check default values
+        assert_eq!(core.current_term(), TERM_0);
+        assert_eq!(core.voted_for(), None);
+
+        // update term to 1
+        let term_updated = core.update_term(TERM_0); // same term
+
+        assert!(!term_updated);
+        assert_eq!(core.current_term(), TERM_0);
+        assert_eq!(core.voted_for(), None);
+    }
+
+    #[test]
+    fn test_core_set_last_applied_increases_greater_index() {
+        let mut core = NodeCore::new(NODE_ID);
+
+        // check default values
+        assert_eq!(core.last_applied(), 0);
+        assert_eq!(core.commit_index(), 0);
+
+        // set commit index to 1 since last applied can't be greater than commit index
+        core.commit_index = 1;
+
+        // set last applied to 1
+        core.set_last_applied(1);
+        assert_eq!(core.last_applied(), 1);
+    }
+
+    #[test]
+    fn test_core_set_last_applied_does_not_increase() {
+        let mut core = NodeCore::new(NODE_ID);
+
+        // check default values
+        assert_eq!(core.last_applied(), 0);
+        assert_eq!(core.commit_index(), 0);
+
+        // set last applied to 1
+        core.set_last_applied(1);
+        assert_eq!(core.last_applied(), 0);
     }
 }
