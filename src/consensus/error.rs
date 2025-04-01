5d7f0b7957c58513a742be7462c2ebdb3c56a750
Serge Kovbasiuk <isserge@protonmail.com>
2025-04-01 11:39:23 +0700

    Update ConsensusError enum

--- src/consensus/error.rs
+++ src/consensus/error.rs
@@ -2,14 +2,14 @@ use crate::messaging::MessagingError;
 
 #[derive(Debug, thiserror::Error, PartialEq, Eq)]
 pub enum ConsensusError {
+    #[error("Timeout: {0}")]
+    Timeout(String),
     #[error("Node {0} not found")]
     NodeNotFound(u64),
     #[error("Node {0} is not a leader")]
     NotLeader(u64),
     #[error("Node {0} is not a candidate")]
     NotCandidate(u64),
-    #[error("Node {0} is not a follower")]
-    NotFollower(u64),
     #[error("Message handling failed: {0}")]
     Transport(#[from] MessagingError),
     // TODO: add log inconsistency error
